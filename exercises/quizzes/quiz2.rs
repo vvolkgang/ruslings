bc41304701c7fdced4eb0619abe9603843a12e09
Álison Fernandes <vvolkgang@users.noreply.github.com>
2026-04-02 20:14:59 +0100

    done - quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,19 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String>{ 
+        let mut output: Vec<String> = Vec::with_capacity(input.len());
+        for (s, c) in input  {
+            let output_string = match c {
+                Command::Uppercase => s.to_uppercase(),
+                Command::Trim => s.trim().to_string(),
+                Command::Append(n) => s + &"bar".repeat(n),
+            };
+            output.push(output_string);
+        }
+        output
+       // [String::from("tets")].to_vec() //test
+    }
 }
 
 fn main() {
@@ -36,8 +48,10 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
